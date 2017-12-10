225355a2fc440c43ddb3a364797752b1f64f4e89
Colin Schulte <pezhead53@gmail.com>
2017-12-10 02:08:44 -0600

    finished

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,8 +21,8 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job job = jobData.Find(id);
+            return View(job);
         }
 
         public IActionResult New()
@@ -35,6 +37,52 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+
+            if (ModelState.IsValid)
+            {
+                SelectListItem newEmployer = new SelectListItem();
+                foreach (var employer in newJobViewModel.Employers)
+                {
+                    if (employer.Value == newJobViewModel.EmployerID.ToString())
+                    {
+                        newEmployer = employer;
+                    }
+                }
+                SelectListItem newLocation = new SelectListItem();
+                foreach (var location in newJobViewModel.Locations)
+                {
+                    if (location.Value == newJobViewModel.LocationID.ToString())
+                    {
+                        newLocation = location;
+                    }
+                }
+                SelectListItem newCoreCompetency = new SelectListItem();
+                foreach (var coreCompetency in newJobViewModel.CoreCompetencies)
+                {
+                    if (coreCompetency.Value == newJobViewModel.CoreCompetencyID.ToString())
+                    {
+                        newCoreCompetency = coreCompetency;
+                    }
+                }
+                SelectListItem newPositionType = new SelectListItem();
+                foreach (var positionType in newJobViewModel.PositionTypes)
+                {
+                    if (positionType.Value == newJobViewModel.PositionTypeID.ToString())
+                    {
+                        newPositionType = positionType;
+                    }
+                }
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = new Employer { ID=newJobViewModel.EmployerID, Value=newEmployer.Text },
+                    Location = new Location { ID = newJobViewModel.LocationID, Value = newLocation.Text },
+                    CoreCompetency = new CoreCompetency { ID = newJobViewModel.CoreCompetencyID, Value = newCoreCompetency.Text },
+                    PositionType = new PositionType { ID = newJobViewModel.PositionTypeID, Value = newPositionType.Text }
+                };
+                jobData.Jobs.Add(newJob);
+                return Redirect("/job?id="+ newJob.ID);
+            }
 
             return View(newJobViewModel);
         }
